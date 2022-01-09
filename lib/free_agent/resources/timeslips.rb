758fc14e3a3ff18be2a7af8a2fbcf6b1ba8cbe6a
Dean Perry <dean@deanpcmad.com>
2022-01-09 15:57:44 +0000

    not sure what happened there

--- lib/free_agent/resources/timeslips.rb
+++ lib/free_agent/resources/timeslips.rb
@@ -1,40 +1,45 @@
 module FreeAgent
-  class UsersResource < Resource
-
-    def me
-      response = get_request("users/me")
-      User.new(response.body["user"])
-    end
+  class TimeslipsResource < Resource
 
     def list(**params)
-      response = get_request("users", params: params)
-      Collection.from_response(response, type: User, key: "users")
+      response = get_request("timeslips", params: params)
+      Collection.from_response(response, type: Timeslip, key: "timeslips")
+    end
+
+    def list_for_user(user:, **params)
+      response = get_request("timeslips?user=#{user}", params: params)
+      Collection.from_response(response, type: Timeslip, key: "timeslips")
+    end
+
+    def list_for_task(task:, **params)
+      response = get_request("timeslips?task=#{task}", params: params)
+      Collection.from_response(response, type: Timeslip, key: "timeslips")
+    end
+
+    def list_for_project(project:, **params)
+      response = get_request("timeslips?project=#{project}", params: params)
+      Collection.from_response(response, type: Timeslip, key: "timeslips")
     end
 
     def retrieve(id:)
-      response = get_request("users/#{id}")
-      User.new(response.body["user"])
+      response = get_request("timeslips/#{id}")
+      Timeslip.new(response.body["timeslip"])
     end
 
-    def create(email:, first_name:, last_name:, role:, opening_mileage: 0,  **params)
-      attributes = {email: email, first_name: first_name, last_name: last_name, role: role, opening_mileage: opening_mileage}
+    def create(task:, user:, project:, dated_on:, hours:,  **params)
+      attributes = {task: task, user: user, project: project, dated_on: dated_on, hours: hours}
 
-      response = post_request("users", body: attributes.merge(params))
-      User.new(response.body["user"]) if response.success?
+      response = post_request("timeslips", body: attributes.merge(params))
+      Timeslip.new(response.body["timeslip"]) if response.success?
     end
 
     def update(id:, **params)
-      response = put_request("users/#{id}", body: params)
-      User.new(response.body["user"]) if response.success?
-    end
-
-    def update_me(**params)
-      response = put_request("users/me", body: {user: params})
-      User.new(response.body["user"]) if response.success?
+      response = put_request("timeslips/#{id}", body: params)
+      Timeslip.new(response.body["timeslip"]) if response.success?
     end
 
     def delete(id:)
-      response = delete_request("users/#{id}")
+      response = delete_request("timeslips/#{id}")
       response.success?
     end
 
