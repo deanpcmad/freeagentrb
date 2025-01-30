fa9cce51ff59f4cf4250378070e9041e489d493e
Dean Perry <dean@deanpcmad.com>
2025-01-30 13:52:46 +0000

    convert amounts to bigdecimal

--- lib/free_agent/objects/bank_transaction.rb
+++ lib/free_agent/objects/bank_transaction.rb
@@ -1,4 +1,21 @@
 module FreeAgent
   class BankTransaction < Object
+    def initialize(attributes)
+      super to_ostruct(attributes)
+
+      # Convert amounts to BigDecimal
+      self.amount = BigDecimal(self.amount.to_s).to_f if self.amount
+      self.unexplained_amount = BigDecimal(self.unexplained_amount.to_s).to_f if self.unexplained_amount
+
+      # Convert amounts in bank_transaction_explanations to BigDecimal
+      if self.bank_transaction_explanations
+        self.bank_transaction_explanations.each do |explanation|
+          explanation.id = explanation.url.match(/\d{2,}/)[0] if explanation.url
+          explanation.gross_value = BigDecimal(explanation.gross_value.to_s).to_f if explanation.gross_value
+          explanation.foreign_currency_value = BigDecimal(explanation.foreign_currency_value.to_s).to_f if explanation.foreign_currency_value
+          explanation.transfer_value = BigDecimal(explanation.transfer_value.to_s).to_f if explanation.transfer_value
+        end
+      end
+    end
   end
 end
